c46e639ff64814cedd2152e4314ace2f45f4b137
Ryan Hartman <ryan.hartman1@gmail.com>
2025-01-05 11:07:48 -0700

    file structure updated, created two true python projects, one for the ui and one for the core

--- VariableIO/rod_end_add.py
+++
@@ -1,9 +0,0 @@
-def add_rod_end(name,radial_load,weight,hole_diameter,shank_diameter,thread):
-    from VariableIO.rod_ends import load_rod_ends
-    from VariableIO.variables import constant
-    if not(name in constant.rod_ends.name):
-        new_line = "\n{},{},{},{},{},{}".format(name,radial_load,weight,hole_diameter,shank_diameter,thread)
-        with open('Lists/rod ends.txt','a') as f_object:
-            f_object.write(new_line)
-            f_object.close()
-        load_rod_ends()
